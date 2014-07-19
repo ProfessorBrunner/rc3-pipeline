9001e7747d94247d4b55d340d4489b49216e29d5
Doris Lee <doris@bigdog-data.astro.illinois.edu>
2014-07-19 09:09:14 -0500

    added nohup.out

--- pipeline_final/bulk_run.py
+++
@@ -1,6 +0,0 @@
-from rc3Catalog import RC3Catalog 
-from sdss import SDSS
-from rc3 import RC3
-rc3cat = RC3Catalog()
-sloan = SDSS()
-rc3cat.mosaicAll(sloan)
--- pipeline_final/twoMass.py
+++
@@ -1,6 +0,0 @@
-# 2MASS Class can not be named starting with number 
-from survey import *
-class TwoMass(Survey):
-    def __init__(self):
-        self.name = '2MASS'
-	
