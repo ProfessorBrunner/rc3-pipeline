e0097a278003c8fa0cec18e2f1b0e5e1ce8b4fd0
dorislee0309 <dorislee@berkeley.edu>
2014-07-18 20:23:59 -0700

    remove except tester

--- pipeline_final/rc3Catalog.py
+++ pipeline_final/rc3Catalog.py
@@ -63,7 +63,7 @@ class RC3Catalog(Catalog):
 				rfits=obj.mosaic_band('r',obj.rc3_ra,obj.rc3_dec,3*obj.rc3_radius,obj.rc3_radius,obj.pgc,survey)
 				if(rfits!=-1): #Special value for outside footprint or error , no rfits produced
 					obj.source_info(rfits,survey)
-			except(ValueError):
+			except:
 				print("Something went wrong in mosaicing PGC {}".format(str(obj.pgc)))
 				while (not (os.path.exists("rc3Catalog.py"))):
 					# if we are not in the Outer directory where rc3Catalog.py Is located at 
