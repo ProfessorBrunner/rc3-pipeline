d83504a28de3bc784a56f88a63a7784cbe7f18ce
dorislee0309 <dorislee@berkeley.edu>
2014-08-01 12:42:34 +0800

    added dss.py sextractor params

--- pipeline_final/dss.py
+++ pipeline_final/dss.py
@@ -6,12 +6,13 @@ class DSS(Survey):
         self.bands=['1b','1r','2b','2r','2ir']
         self.color_bands=['2b','2r','2ir']
         self.best_band ='2r'
-        # self.pixel_size = 2.0 
         self.data_server = Survey._initServer(self)
+        # 1.0 arcsec/pix for POSS-I survey
+        # 1.7 arcsec/pix for POSS-II survey  
+        # Since we are doing astrometry on 2r (best_band), we use PIXEL_SCALE=1.7
+        self.pixel_scale = 1.7
         # Mosaic Program Settings
-        #would it be better OOP practice if I hard code this into dictionary ,or just make a new configuration file for each file and store the config filename here as string then pass it in.
-        # self.sextractor_params={} 
-        #self.montage_params={} # There is no survey depenedent params for montage, fot our purposes montage is purely used for mosaic geometry
-        self.stiff_param_low = " -MAX_TYPE QUANTILE  -MAX_TYPE QUANTILE  -MAX_LEVEL 0.997 -COLOUR_SAT  7 -MIN_TYPE QUANTILE -MIN_LEVEL 1  -GAMMA_FAC 0.7 "
-        self.stiff_param_best = " -MAX_TYPE QUANTILE  -MAX_LEVEL 0.99 -COLOUR_SAT  5  -MIN_TYPE QUANTILE -MIN_LEVEL 1 -GAMMA_FAC 0.8" 
-        # super(SDSS,self).__init__(name,bands,color_bands,best_band,pixel_size,sextractor_params,montage_params,stiff_params)
+        self.sextractor_params= " -PIXEL_SCALE {}".format(self.pixel_scale)
+        # Changes to STIFF param tested in 07-31-2014 Notebook 
+        self.stiff_param_low = " -COPY_HEADER Y -MAX_TYPE QUANTILE   -MAX_LEVEL 0.99 -COLOUR_SAT  1 -MIN_TYPE QUANTILE -MIN_LEVEL 0.001  -GAMMA_FAC 1 "
+        self.stiff_param_best = " -COPY_HEADER Y -MAX_TYPE QUANTILE  -MAX_LEVEL 1 -COLOUR_SAT  1 -MIN_TYPE QUANTILE -MIN_LEVEL 0.7 -GAMMA_FAC 0.9" 
