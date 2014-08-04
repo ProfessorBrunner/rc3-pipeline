36f89e7da395129515e5ddd709c129f102887c26
dorislee0309 <dorislee@berkeley.edu>
2014-08-04 23:37:53 +0800

    fixes to database files , no path created for ones that lie outside survey footprint

--- database/rc3_database.py
+++ database/rc3_database.py
@@ -1,11 +1,11 @@
-# Remember to loop through the rc3.db everytime you rerun this. Database files (.db) can only be instantiated once.
+# Remember to remove rc3.db everytime you rerun this. Database files (.db) can only be instantiated once.
 import sqlite3
 conn = sqlite3.connect("rc3.db")
 c = conn.cursor()
 def tableCreate():
 	#this can only be done once
 	# create table and specify column, primary key is auto incremnting ID number
-	c.execute("CREATE TABLE rc3 (ID INT , PGC_number INT,ra REAL, dec REAL,radius REAL,new_ra REAL,new_dec REAL,new_radius REAL, in_SDSS_footprint BIT ,clean BIT, error INT,PRIMARY KEY(ID))")
+	c.execute("CREATE TABLE rc3 (ID INT , PGC INT,rc3_ra REAL, rc3_dec REAL,rc3_radius REAL,new_ra REAL,new_dec REAL,new_radius REAL, ufits TEXT, gfits TEXT, rfits TEXT ,ifits TEXT, zfits TEXT, best TEXT, low TEXT,in_SDSS_footprint BIT ,clean BIT, error INT,PRIMARY KEY(ID))")
 	#Using BIT to represent boolean 1=True and False = 0
 	
 def dataEntry():
@@ -14,14 +14,14 @@ def dataEntry():
 	n=0
 	with open("rc3_ra_dec_diameter_pgc.txt",'r') as f:
 		for line in f:
-			ra = float(line.split()[0])
-			dec = float(line.split()[1])
-			radius = float(line.split()[2])/2. #diameter -->radius
+			rc3_ra = float(line.split()[0])
+			rc3_dec = float(line.split()[1])
+			rc3_radius = float(line.split()[2])/2. #diameter -->radius
 			pgc=int(str(line.split()[3]).replace(' ', ''))
 
 			with open("rc3_galaxies_outside_SDSS_footprint")as fp:
 				in_SDSS_footprint=1
-				for i in fp: 
+				for i in fp:  
 					if (int(i.split()[3])==pgc):
 						in_SDSS_footprint=0
 
@@ -43,21 +43,29 @@ def dataEntry():
 						new_ra = float(line[2])
 						new_dec = float(line[3])
 						new_radius = float(line[4])
-
 			# Path to Data Products
-			path = "/Volumes/data/rc3/{}/{}".format(pgc,survey)
-			# Scientifically calibrated fits are named in form
-			#  "{}_{}_{}_{}.fits".format(survey.name,band,ra,dec)
-			#  where ra,dec are new positional values passed into mosaic_band
-			#  NEED TO PASS IN SURVEY NAME LATER
-			u_fits = "{}{}_{}_{}_{}.fits".(path,SURVEY,'u',new_ra,new_dec)
-			g_fits = "{}{}_{}_{}_{}.fits".(path,SURVEY,'g',new_ra,new_dec)
-			r_fits = "{}{}_{}_{}_{}.fits".(path,SURVEY,'r',new_ra,new_dec)
-			i_fits = "{}{}_{}_{}_{}.fits".(path,SURVEY,'i',new_ra,new_dec)
-			z_fits = "{}{}_{}_{}_{}.fits".(path,SURVEY,'z',new_ra,new_dec)
-			# -OUTFILE_NAME  {2}_{0}_{1}_BEST.tiff {7}".format(ra,dec,survey.name,
-			best = "{}{}_{0}_{1}_BEST.tiff ".format(path,SURVEY,ra,dec)
-			low  =  "{}{}_{0}_{1}_LOW.tiff ".format(path,SURVEY,ra,dec)
+			if (in_SDSS_footprint==1):
+				# Write only if covered by survey
+				path = "/Volumes/data/rc3/{}/{}/".format(pgc,survey)
+				# Scientifically calibrated fits are named in form
+				#  "{}_{}_{}_{}.fits".format(survey.name,band,ra,dec)
+				#  where ra,dec are new positional values passed into mosaic_band
+				ufits = "{}{}_{}_{}_{}.fits".format(path,SURVEY,'u',new_ra,new_dec)
+				gfits = "{}{}_{}_{}_{}.fits".format(path,SURVEY,'g',new_ra,new_dec)
+				rfits = "{}{}_{}_{}_{}.fits".format(path,SURVEY,'r',new_ra,new_dec)
+				ifits = "{}{}_{}_{}_{}.fits".format(path,SURVEY,'i',new_ra,new_dec)
+				zfits = "{}{}_{}_{}_{}.fits".format(path,SURVEY,'z',new_ra,new_dec)
+				# -OUTFILE_NAME  {2}_{0}_{1}_BEST.tiff {7}".format(ra,dec,survey.name,
+				best = "{}{}_{}_{}_BEST.tiff ".format(path,SURVEY,new_ra,new_dec)
+				low  =  "{}{}_{}_{}_LOW.tiff ".format(path,SURVEY,new_ra,new_dec)
+			else:
+				ufits = ""
+				gfits = ""
+				rfits = ""
+				ifits = ""
+				zfits = ""
+				best = ""
+				low = ""
 
 
 			# Error Information
@@ -97,8 +105,10 @@ def dataEntry():
 					line = i.split()
 					if (int(line[3])==pgc):
 						error=5
-			print (n,ra,dec,radius,pgc,new_ra, new_dec, new_radius,in_SDSS_footprint,clean,error)
-			c.execute("INSERT INTO rc3 VALUES({},{},{},{},{},{},{},{},{},{},{})".format(n,pgc,ra,dec,radius,new_ra,new_dec,new_radius,in_SDSS_footprint,clean,error))
+			print (n)
+			# print (n,ra,dec,radius,pgc,new_ra, new_dec, new_radius,ufits,gfits,rfits,ifits,zfits,best,low,in_SDSS_footprint,clean,error)
+			# Prevent SQL injection and path quoting issue use this notation instead
+ 			c.execute("INSERT INTO rc3 (ID,PGC,rc3_ra,rc3_dec,rc3_radius,new_ra, new_dec, new_radius,ufits,gfits,rfits,ifits,zfits,best,low,in_SDSS_footprint,clean,error)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(n,pgc,rc3_ra,rc3_dec,rc3_radius,new_ra,new_dec,new_radius,ufits,gfits,rfits,ifits,zfits,best,low,in_SDSS_footprint,clean,error))
 			n+=1
 			conn.commit()
 if __name__ == "__main__":
