f11d93741c74251aa051391318e16803224198ea
Doris Lee <doris@bigdog-data.astro.illinois.edu>
2014-08-13 08:59:48 -0500

    added files that was erased accidentally in rm -r * at root

---
+++ pipeline_final/catalog.py
@@ -0,0 +1,34 @@
+
+m abc import ABCMeta , abstractmethod
+
+class Catalog(object):
+	'''
+	Class for Catalog objects
+	'''
+	__metaclass__= ABCMeta
+
+	def __init__(self):
+		self.name= 'Catalog'
+		self.frame= 'fk5'
+		self.equinox = 'J2000'
+		self.allObj=_initAllObj()
+
+	@abstractmethod
+	def _initAllObj(self):
+		'''
+		Create a list of all objects inside the catalog
+		'''
+		raise NotImplementedError()
+
+
+
+	@abstractmethod
+	def mosaicAll(self,survey):
+		'''
+		survey : Survey object
+		Produce all band FITS files and color mosaic for every objects inside the Catalog that lies within the footprint of the given survey
+		'''          
+		raise NotImplementedError()
+
+	def printAll(self):
+		pass
