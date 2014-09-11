b0d394eb896c5ad2c0eae90bfd34a016c9f8154b
dorislee0309 <dorislee@berkeley.edu>
2014-09-11 09:05:57 -0700

    finished script

--- survey_merge.py
+++ survey_merge.py
@@ -1,15 +1,24 @@
 ''' 
 This script merges the data product output 
-[pgc_number]-->dss-->[files] from each survey 
+[pgc_number]-->[files] from each survey 
 to the general catalog format 
 [pgc_number]-->dss,sdss,2mass..etc-->[files]
 '''
 import os
 import shutil
-NUM_DSS_IN_SDSS_COUNT =0
-for i in os.listdir("dss/"):
+ADDITIONAL_NUM_DSS_COUNT =0
+for i in os.listdir("dss_data/"):
 	print i 
 	if os.path.exists("rc3/{}".format(i)):
-		#object already exist (covered by other surveys), just copy the dss directory there
-		NUM_DSS_IN_SDSS_COUNT +=1
-		shutil.move("dss/{}".format(i), "rc3/{}".format(i))
+		#object already exist (covered by other surveys), just copy the dss directory there		
+		pass
+	else:
+		# object doesn't exist, start a new survey
+		ADDITIONAL_NUM_DSS_COUNT +=1
+		#Actually the directory structure is [pgc_number]-->[files] so I need to create dss folder
+		os.mkdir("dss")
+		for j in glob.glob("rc3/{}/*".format(i)):                                                                                                                                      
+			shutil.copy(j, "dss")
+		os.mkdir("rc3/{}".format(i))
+	shutil.move("dss_data/{}".format(i), "rc3/{}/".format(i))
+print ADDITIONAL_NUM_DSS_COUNT
