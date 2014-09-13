477d7a567763708bb71bb3615ec699d1668a902b
dorislee0309 <dorislee@berkeley.edu>
2014-09-13 09:55:34 -0700

    completeed survey_merge script, should be able to run on bigdog now, becareful of the chmod set on bigdog rc3

--- survey_merge.py
+++ survey_merge.py
@@ -1,24 +1,30 @@
-''' 
-This script merges the data product output 
-[pgc_number]-->[files] from each survey 
-to the general catalog format 
+'''
+This script merges the data product output
+[pgc_number]-->[files] from each survey
+to the general catalog format
 [pgc_number]-->dss,sdss,2mass..etc-->[files]
 '''
 import os
+import glob
 import shutil
 ADDITIONAL_NUM_DSS_COUNT =0
-for i in os.listdir("dss_data/"):
-	print i 
-	if os.path.exists("rc3/{}".format(i)):
-		#object already exist (covered by other surveys), just copy the dss directory there		
-		pass
-	else:
-		# object doesn't exist, start a new survey
-		ADDITIONAL_NUM_DSS_COUNT +=1
-		#Actually the directory structure is [pgc_number]-->[files] so I need to create dss folder
-		os.mkdir("dss")
-		for j in glob.glob("rc3/{}/*".format(i)):                                                                                                                                      
-			shutil.copy(j, "dss")
-		os.mkdir("rc3/{}".format(i))
-	shutil.move("dss_data/{}".format(i), "rc3/{}/".format(i))
+# for i in os.listdir("dss_data/"):
+# eliminate weird files like .DSStore and other php and html files
+for i in [name for name in os.listdir("dss_data") if os.path.isdir(os.path.join("dss_data", name)) ]:
+     print i
+     if os.path.exists("rc3/{}".format(i)):
+          #object already exist (covered by other surveys), just copy the dss directory there        
+          os.mkdir("dss_data/dss")
+          for j in glob.glob("dss_data/{}/*".format(i)):
+               shutil.copy(j, "dss_data/dss")
+          shutil.move("dss_data/dss", "rc3/{}/".format(i))
+     else:
+          # object doesn't exist, start a new survey
+          ADDITIONAL_NUM_DSS_COUNT +=1
+          #Actually the directory structure is [pgc_number]-->[files] so I need to create dss folder
+          os.mkdir("dss_data/dss")
+          for j in glob.glob("dss_data/{}/*".format(i)):
+               shutil.copy(j, "dss_data/dss")
+          os.mkdir("rc3/{}".format(i))
+          shutil.move("dss_data/dss", "rc3/{}/".format(i))
 print ADDITIONAL_NUM_DSS_COUNT
