b98584f27e69ac87465fd931725f32fc8ab2909e
Doris Lee <doris@bigdog-data.astro.illinois.edu>
2015-03-24 02:35:13 -0500

    Improved on post_analysis code so that the radius is also written in with the associated coordinates and magnitudes; Using a trick to implement this that could be generalized to any variable that I want to put in in the future. Basically, ignore the whole source matching algoirthm, we link together variable X and Y in the beginning, (here radius and magnitude), let the source matching algorithm do its magic to match coord to magnitudes, then we just crunch through the XY list from the beginning and match the X and Y together, which would automatically imply that the coordinates are matched

--- test/post_analysis.py
+++ test/post_analysis.py
@@ -9,8 +9,9 @@ import glob
 import os
 import shutil
 import matplotlib.pyplot as plt
+# PGC = 243
 for PGC in os.walk('.').next()[1][1:]:
-    print PGC
+    print "PGC: {}".format(PGC)
     for i in glob.glob("default.*"):
         shutil.copy(i,"{}/".format(PGC))
     os.chdir(str(PGC))
@@ -49,8 +50,7 @@ for PGC in os.walk('.').next()[1][1:]:
     out_coord = np.array(out_coord)
     out_ra_lst = out_coord[::,0]
     out_dec_lst = out_coord[::,1]
-
-
+    
     os.chdir("r")
     for r_band_inputs in all_r_input:
         os.system("sextractor  {}".format(r_band_inputs))
@@ -84,8 +84,8 @@ for PGC in os.walk('.').next()[1][1:]:
     mag_lst = np.array(mag_lst)
     idx = np.where(abs(mag_lst-out_mag_lst[0])<0.1)
     epsilon = 0.001#small margin
-    idx_ra = np.intersect1d(np.where(min(out_ra_lst)<coord[::,0]+epsilon),np.where(coord[::,0]<max(out_ra_lst)+epsilon))
-    idx_dec = np.intersect1d(np.where(min(out_dec_lst)<coord[::,1]+epsilon),np.where(coord[::,1]<max(out_dec_lst)+epsilon))
+    idx_ra =np.intersect1d(np.where(min(out_ra_lst)<coord[::,0]+epsilon)[0],np.where(coord[::,0]<max(out_ra_lst)+epsilon)[0])
+    idx_dec = np.intersect1d(np.where(min(out_dec_lst)<coord[::,1]+epsilon)[0],np.where(coord[::,1]<max(out_dec_lst)+epsilon)[0])
     intersect = np.intersect1d(idx_ra,idx_dec)
     input_within_region  = coord[intersect]
     input_within_region_with_mag =np.c_[input_within_region, mag_lst[intersect]] 
@@ -104,31 +104,33 @@ for PGC in os.walk('.').next()[1][1:]:
     matched_mag_lst_output =[]
     if len(matched_coord)==0:
         print ("No matched sources for PGC {}".format(PGC))
-        os.getcwd()
-        os.chdir("../../")
+        print(os.getcwd())
+	os.chdir("../../")
+	os.system("mv {} ../500no_matched/".format(PGC))
         continue
     for i in matched_coord[::,0]:
         for j in out_coord_mag:
             if i[0]==j[0]:
-		print "out matched_coord",i 
-		print "out_coord_mag", j
-                matched_mag_lst_output.append(j[2])
+	        print "matched_coord: ",i
+                print "output_within_region_with_mag:",j
+                matched_mag_lst_output.append(j)
     # Matching coordinates with input magnitudes
     matched_mag_lst_input =[]
-
     for i in matched_coord[::,1]:
         for j in input_within_region_with_mag:
             if i[0]==j[0]:
-                print "in  matched_coord",i
-                print "in_coord_mag", j
-                matched_mag_lst_input.append(j[2])
-    plt.plot(matched_mag_lst_output,matched_mag_lst_input,'o')
+		print "matched_coord: ",i
+		print "input_within_region_with_mag:",j
+                matched_mag_lst_input.append(j)
+    #plt.plot(matched_mag_lst_output,matched_mag_lst_input,'o')
     os.chdir("../..")
     with open("input_mag","a") as in_file:
         # np.savetxt(mag_file,(matched_mag_lst_input,matched_mag_lst_output))
         np.savetxt(in_file,matched_mag_lst_input)
     with open("output_mag","a") as out_file:
         np.savetxt(out_file,matched_mag_lst_output)
+    os.system("mv {} ../500finished_post_analysis".format(PGC))    
+#shutil.move(PGC,"finished_post_analysis/")
     # np.savetxt("input_output_mag",)
 
 # plt.plot(-np.arange(10),-np.arange(10)) #Slope seems to be 1 but off by a constant offset
