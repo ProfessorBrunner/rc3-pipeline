982546492a3555945c03cdeed7030b29745ecdc9
Doris Lee <dorislee@berkeley.edu>
2015-07-21 11:39:48 -0700

    find bug in sdss.py, redundant flag value

--- list_pgc.sh
+++ list_pgc.sh
@@ -2,6 +2,7 @@ for i in RUN*
   do
     echo "Working on $i"
     cd $i
+    rm -r r/
     ls -d */ | cut -f1 -d'/' >> ../pgc_done.txt
     cd ..
   done
--- pipeline/sdss.py
+++ pipeline/sdss.py
@@ -9,7 +9,7 @@ class SDSS(Survey):
         self.pixel_scale =  0.396 #(arcsec/pix)
         # Mosaic Program Settings
         self.sextractor_params= " -PIXEL_SCALE {}".format(self.pixel_scale)
-        self.stiff_param_low = " -MAX_TYPE QUANTILE  -MAX_TYPE QUANTILE  -MAX_LEVEL 0.997 -COLOUR_SAT  7 -MIN_TYPE QUANTILE -MIN_LEVEL 1  -GAMMA_FAC 0.7 "
+        self.stiff_param_low = " -MAX_TYPE QUANTILE  -MAX_LEVEL 0.997 -COLOUR_SAT  7 -MIN_TYPE QUANTILE -MIN_LEVEL 1  -GAMMA_FAC 0.7 "
         self.stiff_param_best = " -MAX_TYPE QUANTILE  -MAX_LEVEL 0.99 -COLOUR_SAT  5  -MIN_TYPE QUANTILE -MIN_LEVEL 1 -GAMMA_FAC 0.8" 
 
 
