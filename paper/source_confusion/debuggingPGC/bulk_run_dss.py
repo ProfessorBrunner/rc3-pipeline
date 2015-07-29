f64613dab392d45e160922f0c92c179b9e0cd859
dorislee0309 <dorislee@berkeley.edu>
2015-07-29 10:01:13 -0400

    cleaned up github repo

--- paper/source_confusion/debuggingPGC/bulk_run_dss.py
+++
@@ -1,6 +0,0 @@
-from rc3Catalog import RC3Catalog 
-from dss import DSS
-from rc3 import RC3
-rc3cat = RC3Catalog()
-dss = DSS()
-rc3cat.mosaicAll(dss)
--- test/catalogSubset.py
+++
@@ -1,9 +0,0 @@
-import os
-from rc3 import RC3
-from rc3Catalog import RC3Catalog
-subset = RC3Catalog()
-subsetlst= subset.initSubset("rc3_subsample.txt")
-subset.allObj = subsetlst
-from sdss import SDSS
-sdss = SDSS()
-subset.mosaicAll(sdss)
